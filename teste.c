14d6fe53def02542d07ee7afe234c2afe3ef4166
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-07 13:01:52 -0200

    00

---
+++ teste.c
@@ -0,0 +1,31 @@
+// Isso é um comentário
+/* Isso é um comentário */
+/*Comentário
+Comentário */
+
+// As linhas abaixo "chamam" as bibliotecas :
+#include <stdio.h>
+// Essa linha é Entrada e saída do sistema
+
+#include <stdlib.h>
+// Essa linha são Funções básicos do sistema
+
+
+int main(int argc, char *argv[]) 
+{
+	printf("Hello World");
+	printf("\nHello World \n");
+	printf("\n Hello World \n");
+	system("pause");
+	return 0;
+}
+// int Números inteiros de -32768 a 32767
+// main ou principal é o ponto de partida . Dentro do main teremos parâmetros
+// argc é número de linhas do código
+// char é um tipo de variável que aceita qualquer coisa ( caracter , letra , número , etc )
+// argv é o conteúdo das linhas do código
+//return é uma função de retorno ao main
+//return 0 indica que o aplicativo retornou sem erros no código
+//print Escreve na tela o texto
+// \n Pula uma linha
+// system Pede pra você apertar algo para continuar
